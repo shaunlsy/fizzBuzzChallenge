d021a2ae69a8aa8ad2549e1332aa514d65d46000
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-20 15:49:19 +0000

    refactor fizzbuzz.js

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -6,37 +6,37 @@ describe("FizzBuzz", function() {
   });
   describe("knows when the number is", function() {
     it("divisible by 3", function() {
-      expect(fizzBuzz.isDivisibleBy3(15)).toBe(true);
+      expect(fizzBuzz.isDivisibleBy(3, 15)).toBe(true);
     });
 
     it("is not divisible by 3", function() {
-      expect(fizzBuzz.isDivisibleBy3(16)).toBe(false);
+      expect(fizzBuzz.isDivisibleBy(3, 16)).toBe(false);
     });
 
     it("is divisible by 5", function() {
-      expect(fizzBuzz.isDivisibleBy5(5)).toBe(true);
+      expect(fizzBuzz.isDivisibleBy(5, 5)).toBe(true);
     });
 
     it("is not divisible by 5", function() {
-      expect(fizzBuzz.isDivisibleBy5(6)).toBe(false);
+      expect(fizzBuzz.isDivisibleBy(5, 6)).toBe(false);
     });
   });
 
-  describe('print the number, Fizz, Buzz or FizzBuzz if', function() {
-    it('is divisible by 3 and 5', function() {
-      expect(fizzBuzz.printFizzBuzz(15)).toMatch('FizzBuzz')
+  describe("print the number, Fizz, Buzz or FizzBuzz if", function() {
+    it("is divisible by 3 and 5", function() {
+      expect(fizzBuzz.printFizzBuzz(15)).toMatch("FizzBuzz");
     });
 
-    it('if divisible by 3', function() {
-      expect(fizzBuzz.printFizzBuzz(3)).toMatch('Fizz');
+    it("if divisible by 3", function() {
+      expect(fizzBuzz.printFizzBuzz(3)).toMatch("Fizz");
     });
 
-    it('if divisible by 5', function() {
-      expect(fizzBuzz.printFizzBuzz(5)).toMatch('Buzz');
+    it("if divisible by 5", function() {
+      expect(fizzBuzz.printFizzBuzz(5)).toMatch("Buzz");
     });
 
-    it('is not divisible by any', function() {
-      expect(fizzBuzz.printFizzBuzz(2)).toMatch('2');
+    it("is not divisible by any", function() {
+      expect(fizzBuzz.printFizzBuzz(2)).toMatch("2");
     });
   });
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,21 +1,31 @@
 var FizzBuzz = function() {};
 
-FizzBuzz.prototype.isDivisibleBy3 = function(number) {
-  return number % 3 === 0;
-};
+// FizzBuzz.prototype.isDivisibleBy3 = function(number) {
+//   return number % 3 === 0;
+// };
 
-FizzBuzz.prototype.isDivisibleBy5 = function(number) {
-  return number % 5 === 0;
+// FizzBuzz.prototype.isDivisibleBy5 = function(number) {
+//   return number % 5 === 0;
+// };
+
+FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
+  return number % divisor === 0;
 };
 
 FizzBuzz.prototype.printFizzBuzz = function(number) {
-  if (this.isDivisibleBy5(number) && this.isDivisibleBy3(number)) {
-    return 'FizzBuzz';
-  } else if (this.isDivisibleBy5(number)) {
-    return 'Buzz';
-  } else if (this.isDivisibleBy3(number)) {
-    return 'Fizz';
+  if (this.isDivisibleBy(15, number)) {
+    return "FizzBuzz";
+  } else if (this.isDivisibleBy(5, number)) {
+    return "Buzz";
+  } else if (this.isDivisibleBy(3, number)) {
+    return "Fizz";
   } else {
     return number.toString();
+  }
+};
+
+FizzBuzz.prototype.print1To100 = function() {
+  for (let i = 0; i <= 100; i++) {
+    console.log(this.printFizzBuzz(i));
   }
 };
